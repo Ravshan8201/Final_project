7ed9fd8904ec64d2886ae7a7d946dff39ef3573c
sulto <sultonovravshan294@gmail.com>
2021-09-27 18:20:12 +0500

    Initial commit

---
+++ cons.py
@@ -0,0 +1,54 @@
+TOKEN = '2049394974:AAHFWfRXAzVUCLe3IOnhRah0fPXTHZtWjws'
+dct = {1:['Здраствуйте, Я Авазбек, являюсь репитером по английскому языку.',#0
+             'Пожалуйта, введите ваше имя:',#1
+             '{}, вы уже есть в нашей базе, нам не хватает вашего номера(\nСкиньте ваш контакт 😘',#2
+              'нажмите на кнопку далее...',#3
+              'Поделиться контактом☎',#4
+              '{}, пожалуйста выберите курс в зависимости от объема вашего знания: ', #5
+              'Вы выбрали курс{}', #6
+              'Имя: {}\nВаш номер: {}\nКурс: {}\nСтоимость: {}\nЖдите скоро с вами свяжемся', #7
+              'Кто такой такой Авазбек Бахтиёров🧐🧐🧐?', #8
+              'Отлично😃😃😃', #9
+              'Авазбек Бахтиёров окончил УзГУМЯ на красный диплом📕. Магистратуру закончил в Университет мировой Экономики и Дипломатии в факультете Международные отношении и мировая политика😎😎😎\nIelts 8.0\nListening: 9.0\nReading: 8.5\nSpeaking: 7.5\nWriting: 7.0\nБолее 5 лет опыта в сфере языкообучения', #10
+              '', #11
+              '', #12
+              '', #13
+              '', #14
+              '', #15
+              '', #16
+             ],
+
+       2: ['Assalomu alaykum, men Avazbekning kursiga ro^yxatdan o^takazishingizga yordamchi va sizning IELTS instruktoringizman.',  # 0
+            'Iltimos, ismingizni kiriting:',  # 1
+            '{}, siz bizning malumotlar bazamizda mavjudsiz, bizga sizning telefon raqamingiz kerak xolos (\nKontaktingizni ulashing 😘',
+            # 2
+            'Davom etish tugmasini bosing...',  # 3
+            'Kontaktni ulashish☎️',  # 4
+            '{}, Iltimos, o’z bilim darajangizga mos ingliz tili kursini tanlang: ',  # 5
+            'Siz kursni tanladingiz{}',  # 6
+            'Ism: {}\nSizning raqamingiz: {}\nKurs: {}\nNarxi: {}\nIltimos biroz sabr qiling tez orada biz siz bilan bog’lanamiz',
+            # 7
+            'Avazbek Baxtiyorov kim o’zi 🧐🧐🧐?',  # 8
+            'Ajoyib😃😃😃',  # 9
+            'Avazbek Baxtiyorov O’zDJTUning ingliz filologiyasi fakultetini qizil diplomga tamomlagan📕. Magistraturani esa Jahon iqtisodiyoti va diplomatiya universitetining Xalqaro munosabatlar va Jahon siyosati fakultetidaa tamomlagan😎😎😎\nIelts 8.0\nListening: 9.0\nReading: 8.5\nSpeaking: 7.5\nWriting: 7.0\nIngliz tilini o’qitish bo’yicha 5 yildan ortiq tajriba',
+            # 10
+            '',  # 11
+            '',  # 12
+            '',  # 13
+            '',  # 14
+            '',  # 15
+            '',  # 16
+            ]}
+
+level_dct = {
+    1: 'Beginner',
+    2: 'Elementary',
+    3: 'Pre-intermediate',
+    4: 'Intermediate',
+    5: 'Pre-ielts',
+    6: 'Ielts',
+    7: ' '
+
+
+
+}
